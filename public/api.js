75c9b324d8da162c1f265c8160f816cab7ae8ebb
MattCross01 <mountaindew2141@outlook.com>
2021-06-08 18:01:38 -0400

    stats.js more legible

--- public/api.js
+++ public/api.js
@@ -1,26 +1,21 @@
 const API={async getLastWorkout()
  
   {const t=await fetch("/api/workouts")
- 
   ,a=await t.json();return a[a.length-1]},
   
   async addExercise(t){const a=location.search.split("=")
-  
   [1],o=await fetch("/api/workouts/"+a,
   
   {method:"PUT",headers:{"Content-Type":"application/json"},
-  
   body:JSON.stringify(t)})
   ;return await o.json()}
   
   ,async createWorkout()
   
   {const t=await fetch("/api/workouts",
-  
   {method:"POST",headers:{"Content-Type":"application/json"}});
   
   return await t.json()}
   
   ,async getWorkoutsInRange()
-  
   {const t=await fetch("/api/workouts/range");return await t.json()}};
--- public/stats.js
+++ public/stats.js
@@ -1,5 +1,20 @@
 function generatePalette()
-{return["#003f5c","#2f4b7c","#665191","#a05195","#d45087","#f95d6a","#ff7c43","ffa600","#003f5c","#2f4b7c","#665191","#a05195","#d45087","#f95d6a","#ff7c43","ffa600"]}
+{return["#003f5c",
+"#2f4b7c",
+"#665191",
+"#a05195",
+"#d45087",
+"#f95d6a",
+"#ff7c43",
+"ffa600",
+"#003f5c",
+"#2f4b7c",
+"#665191",
+"#a05195",
+"#d45087",
+"#f95d6a",
+"#ff7c43",
+"ffa600"]}
 
 function populateChart(e){let a=duration(e),
   t=calculateTotalWeight(e),
@@ -12,7 +27,13 @@ function populateChart(e){let a=duration(e),
    n=document.querySelector("#canvas3").getContext("2d"),
    l=document.querySelector("#canvas4").getContext("2d");
    
-   new Chart(s,{type:"line",data:{labels:["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"],
+   new Chart(s,{type:"line",data:{labels:["Sunday",
+   "Monday",
+   "Tuesday",
+   "Wednesday",
+   "Thursday",
+   "Friday",
+   "Saturday"],
    
    datasets:[{label:"Workout Duration In Minutes",
    backgroundColor:"red",
@@ -22,15 +43,41 @@ function populateChart(e){let a=duration(e),
    scales:{xAxes:[{display:!0,scaleLabel:{display:!0}}],
    yAxes:[{display:!0,scaleLabel:{display:!0}}]}}}),
    
-   new Chart(d,{type:"bar",data:{labels:["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"],
+   new Chart(d,{type:"bar",data:{labels:["Sunday",
+   "Monday",
+   "Tuesday",
+   "Wednesday",
+   "Thursday",
+   "Friday",
+   "Saturday"],
    
-   datasets:[{label:"Pounds",data:t,backgroundColor:["rgba(255, 99, 132, 0.2)","rgba(54, 162, 235, 0.2)","rgba(255, 206, 86, 0.2)","rgba(75, 192, 192, 0.2)","rgba(153, 102, 255, 0.2)","rgba(255, 159, 64, 0.2)"],
-   borderColor:["rgba(255, 99, 132, 1)","rgba(54, 162, 235, 1)","rgba(255, 206, 86, 1)","rgba(75, 192, 192, 1)","rgba(153, 102, 255, 1)","rgba(255, 159, 64, 1)"],
-   borderWidth:1}]},options:{title:{display:!0,text:"Pounds Lifted"},
-   scales:{yAxes:[{ticks:{beginAtZero:!0}}]}}}),
+   datasets:[{label:"Pounds",data:t,backgroundColor:["rgba(255, 99, 132, 0.2)",
+   "rgba(54, 162, 235, 0.2)",
+   "rgba(255, 206, 86, 0.2)",
+   "rgba(75, 192, 192, 0.2)",
+   "rgba(153, 102, 255, 0.2)",
+   "rgba(255, 159, 64, 0.2)"],
+   
+   borderColor:["rgba(255, 99, 132, 1)",
+   "rgba(54, 162, 235, 1)",
+   "rgba(255, 206, 86, 1)",
+   "rgba(75, 192, 192, 1)",
+   "rgba(153, 102, 255, 1)",
+   "rgba(255, 159, 64, 1)"],
+   
+   borderWidth:1}]},
+   options:
+   {title:
+    {display:!0,
+      text:"Pounds Lifted"},
+      scales:{yAxes:
+    [{ticks:{beginAtZero:!0}}]}}}),
    
    new Chart(n,{type:"pie",data:{labels:r,datasets:
-    [{label:"Excercises Performed",backgroundColor:o,data:a}]},options:{title:{display:!0,text:"Excercises Performed"}}}),
+    [{label:"Excercises Performed",
+    backgroundColor:o,data:a}]},
+    options:{title:{display:!0,
+      text:"Excercises Performed"}}}),
     
     new Chart(l,{type:"doughnut",
     data:{labels:r,datasets:[{label:"Excercises Performed",
